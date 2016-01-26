b2f7cc9adf8b96ca281c83a13cdb99a02a7a9dce
sina7skk <hacker.7sk.33@gmail.com>
2016-01-26 20:11:55 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {51718050},
+    sudo_users = {114934518,104878785},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
